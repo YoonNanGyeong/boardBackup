1f273abcacac99c6ce9d0ee78ed69d5af2a4cebf
YoonNanGyeong <nangyung97@gmail.com>
2023-07-11 18:12:41 +0900

    sql 매핑 파일명 변경

--- src/main/java/egovframework/normal/board/service/impl/SampleDAO.java
+++ src/main/java/egovframework/normal/board/service/impl/SampleDAO.java
@@ -23,23 +23,6 @@ import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
 
 import org.springframework.stereotype.Repository;
 
-/**
- * @Class Name : SampleDAO.java
- * @Description : Sample DAO Class
- * @Modification Information
- * @
- * @  수정일      수정자              수정내용
- * @ ---------   ---------   -------------------------------
- * @ 2009.03.16           최초생성
- *
- * @author 개발프레임웍크 실행환경 개발팀
- * @since 2009. 03.16
- * @version 1.0
- * @see
- *
- *  Copyright (C) by MOPAS All right reserved.
- */
-
 
 @Repository("sampleDAO")
 public class SampleDAO extends EgovAbstractDAO {
