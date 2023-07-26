a8df49bad54d6c0574aee22ea226d3cd27ec449b
YoonNanGyeong <nangyung97@gmail.com>
2023-07-26 14:22:08 +0900

    paging 이미지 변경

--- src/main/java/egovframework/normal/cmmn/web/ImgPaginationRenderer.java
+++ src/main/java/egovframework/normal/cmmn/web/ImgPaginationRenderer.java
@@ -20,12 +20,12 @@ public class ImgPaginationRenderer extends AbstractPaginationRenderer implements
 	*/
 	public void initVariables() {
 
-		firstPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/board/cmmn/btn_page_pre10.gif' border=0/></a>&#160;";
-		previousPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/board/cmmn/btn_page_pre1.gif' border=0/></a>&#160;";
-		currentPageLabel = "<strong>{0}</strong>&#160;";
-		otherPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">{2}</a>&#160;";
-		nextPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/board/cmmn/btn_page_next1.gif' border=0/></a>&#160;";
-		lastPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/board/cmmn/btn_page_next10.gif' border=0/></a>&#160;";
+		firstPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/board/cmmn/btn_page_next1.png' border=0/></a>&#160;";
+		previousPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/board/cmmn/btn_page_pre1.png' border=0/></a>&#160;";
+		currentPageLabel = "<span><strong>{0}</strong></span>&#160;";
+		otherPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\"><button type=\"button\" class=\"page-btns\">{2}</button></a>&#160;";
+		nextPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/board/cmmn/btn_page_next10.png' border=0/></a>&#160;";
+		lastPageLabel = "<a href=\"javascript:void(0)\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/board/cmmn/btn_page_pre10.png' border=0/></a>&#160;";
 	}
 
 	@Override
