910ed048406c66941bf50f991f08c83eed0dfa9e
YoonNanGyeong <nangyung97@gmail.com>
2023-08-02 18:07:23 +0900

    수정화면에서 다운로드 기간제한 부분 삭제

--- src/main/java/egovframework/normal/board/web/BoardController.java
+++ src/main/java/egovframework/normal/board/web/BoardController.java
@@ -414,30 +414,6 @@ public class BoardController {
 		model.addAttribute("fileSize",fileList.size());
 		
 		
-		String updateDt = selectedVO.getUpdateDt(); // 최근 글 작성 날짜(시간)
-		
-		LocalDate now = LocalDate.now();	 // 현재 날짜
-		updateDt = updateDt.replace(".", "-");
-		String[] parts = updateDt.split(" "); 
-		String result = parts[0];  // 최근 글 작성 날짜에서 시간 제거
-		LocalDate resultDt = LocalDate.parse(result);	// 문자열 -> 날짜 타입
-		
-		long daysDifference = ChronoUnit.DAYS.between(resultDt, now);	// 날짜 일수 차이 계산
-//		System.out.println("날짜 일수 차이: " + daysDifference);
-		
-		Boolean dateResult = true;
-		
-		if(daysDifference > 30) {
-			dateResult = false;
-		}else {
-			dateResult = true;
-		}
-		
-//		System.out.println("dateResult = "+dateResult);
-		
-		model.addAttribute("dateResult", dateResult);
-		
-		
 		return "board/board_register";
 	}
 
--- src/main/webapp/WEB-INF/jsp/egovframework/normal/board/board_register.jsp
+++ src/main/webapp/WEB-INF/jsp/egovframework/normal/board/board_register.jsp
@@ -106,7 +106,7 @@
 
 	           			<!-- 기존에 첨부된 파일이 있으면 표시  -->   
 						<!-- 수정일 기준 30일 경과 되면 다운로드 안됨 -->
-						<c:if test="${fileSize gt 0 && dateResult eq true}">			
+						<c:if test="${fileSize gt 0}">			
 				                <div class="download-area">
 		                		  <label for="fileSq" style="margin-bottom: 20px;">
 		                		  	첨부파일 다운로드
